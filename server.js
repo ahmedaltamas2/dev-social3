93f602082e528f7723517f0a18b384aa1753c9eb
altamas ahmed <ahmedaltamas2@gmail.com>
2021-07-22 20:18:46 +0530

    deploy attempt 6

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use('/api/auth',require('./routes/api/auth'));
 
 
 //static file in production
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('client/build'));
+//     app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-    })
-}
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+//     })
+// }
 
 const PORT = process.env.PORT || 5000;
 
